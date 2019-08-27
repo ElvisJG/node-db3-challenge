5de2e00bb4e84e1b2bca5b96a7be2381c530e9c7
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-27 19:15:45 -0400

    initial boilerplate to connect to the db using knex and exports stack on model

---
+++ schemes/scheme-model.js
@@ -0,0 +1,22 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {}
+
+function findById() {}
+
+function findSteps() {}
+
+function add() {}
+
+function update() {}
+
+function remove() {}
